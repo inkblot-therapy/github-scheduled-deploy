3481008177791a55952bcd185578a2da3446b64a
Audee <amvelasco@gmail.com>
2021-03-11 14:49:49 -0500

    basic Toolkit.run

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import { Toolkit } from 'actions-toolkit'
 
 Toolkit.run(async (tools) => {
+  const schedule = process.env.SCHEDULE
+  const webhook = process.env.MEDSTACK_WEBHOOK
 
+  try {
+    if(!webhook) {
+      // TODO: throw an error
+    }
+  } catch (e) {
+    tools.log.fatal(e);
+    tools.exit.failure('Failed Scheduled Deploy');
+  }
+  tools.exit.success('Deploy is Schedule');
 });
