e06301ec9a57af52b6df553e4a90dd81743cca03
Audee <amvelasco@gmail.com>
2021-03-11 16:06:45 -0500

    adding the logic

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
 import { Toolkit } from 'actions-toolkit'
+import request from 'async-request';
+import scheduler from 'node-schedule';
+import moment from 'moment'
 
 Toolkit.run(async (tools) => {
-  const schedule = process.env.SCHEDULE
-  const webhook = process.env.MEDSTACK_WEBHOOK
+  const schedule = moment().endOf('day').add(2, 'hours').toDate();
+  const webhook = process.env.MEDSTACK_WEBHOOK;
 
   try {
     if(!webhook) {
-      // TODO: throw an error
+      throw new Error('Webhoook is required.');
     }
+    scheduler.scheduleJob(schedule, () => {
+      const res = await request(webhook);
+      if (res.status != 200) {
+        throw new Error(`Returned ${ res.status }`);
+      }
+    });
   } catch (e) {
     tools.log.fatal(e);
-    tools.exit.failure('Failed Scheduled Deploy');
+    tools.exit.failure(e);
   }
-  tools.exit.success('Deploy is Schedule');
+  tools.exit.success('Deploy was initiated.');
 });
