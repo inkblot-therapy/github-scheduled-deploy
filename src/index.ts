30addb285cc213727fbeda78b1e5194595614f99
Audee <amvelasco@gmail.com>
2021-03-12 18:24:21 -0500

    natively using set timeout

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,28 @@
 import { Toolkit } from 'actions-toolkit'
 import request from 'async-request';
-import scheduler from 'node-schedule';
 import moment from 'moment'
 
+async function suspend(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
+
 Toolkit.run(async (tools) => {
-  const schedule = moment().add(5, 'minutes').toDate();
-  // const schedule = moment().endOf('day').add(2, 'hours').toDate();
+  const schedule = moment().endOf('day').add(2, 'hours').valueOf() - moment().valueOf();
   const webhook = process.env.MEDSTACK_WEBHOOK;
 
   try {
     if(!webhook) {
       throw new Error('Webhoook is required.');
     }
-
-    await scheduler.scheduleJob(schedule, async () => {
-      const res = await request(webhook);
-      console.log(res);
-
-      if (res.status != 200) {
-        throw new Error(`Returned ${ res.status }`);
-      }
-    });
+    console.log(schedule);
+    await suspend(schedule);
+    const res = await request(webhook, { method: 'POST' });
+    if (res.statusCode != 200) {
+      throw new Error(`Returned ${ res.status }`);
+    }
+    console.log(res);
   } catch (e) {
     tools.log.fatal(e);
     tools.exit.failure(e);
